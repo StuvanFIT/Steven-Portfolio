17a926916198e39de7462e9847d4b34c1e2f4ef2
skai0008 <kaingsteven123@gmail.com>
2025-07-10 22:29:14 +1000

    testing.

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,16 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-      function App() {
+          function App() {
 
 
 
 
   const [count, setCount] = useState(0);
-
-
-
-
 
 
 
