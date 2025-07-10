81439ecfd55a6f254ac9c5b4798babb4ac6ffb1e
skai0008 <kaingsteven123@gmail.com>
2025-07-10 23:21:26 +1000

    testing husky

--- portfolio/eslint.config.js
+++ portfolio/eslint.config.js
@@ -6,19 +6,29 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
 export default tseslint.config([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      prettierConfig,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      'prettier/prettier': 'error',
     },
   },
 ])
--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,36 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-    function App() {
-                const [count, setCount] = useState(0);
-
-
-
-
-
-
-
-
-
-
-                
-
-
-
-
+            function App() {
+  const [count, setCount] = useState(0)
 
   return (
-
-
-
-
-
-
-
-
-
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -53,7 +29,7 @@ import './App.css';
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
