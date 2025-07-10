6dc3a9dc0f1df902134cd5ecf53b89eb3a185154
skai0008 <kaingsteven123@gmail.com>
2025-07-10 22:25:19 +1000

    testing husky hook

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-                function App() {
+      function App() {
 
 
 
@@ -15,7 +15,7 @@ import "./App.css";
 
 
 
-  
+
 
   return (
     <>
