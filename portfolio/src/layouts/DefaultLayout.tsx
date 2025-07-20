1ab1cee491fc0a735febf0c0af0b16551771cae3
skai0008 <skai0008@student.monash.edu>
2025-07-21 01:46:27 +1000

    update: removed unused imports; lint-fix

--- portfolio/src/layouts/DefaultLayout.tsx
+++ portfolio/src/layouts/DefaultLayout.tsx
@@ -1,22 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
+import { Outlet } from 'react-router-dom'
 
+import Header from '../components/Header'
 
-const DefaultLayout = () =>{
-
-    return (
-        <div className='min-h-screen bg-black text-white relative overflow-hidden font-mono'>
-            <Header />
-            <main className='flex-1'>
-                <Outlet/>
-            </main>
-        </div>
-
-    )
-
-
+const DefaultLayout = () => {
+  return (
+    <div className="min-h-screen bg-black text-white relative overflow-hidden font-mono">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  )
 }
-
 
 export default DefaultLayout
